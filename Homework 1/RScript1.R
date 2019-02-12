857b8383926d874b6d18528e7164bd2f586197df
Vignesh M Pagadala <vignesh.pagadala@colostate.edu>
2019-02-11 23:22:09 -0700

    Commit

--- Homework 1/RScript1.R
+++ Homework 1/RScript1.R
@@ -14,3 +14,27 @@ cor(InData, method = c('pearson'))
 protein_model <- lm(TIME ~ PROTEIN, data = InData)
 protein_model
 summary(protein_model)
+
+# Q3.(b) Time as a response to Antibio
+antibio_model <- lm(TIME ~ ANTIBIO, data = InData)
+antibio_model
+summary(antibio_model)
+
+# Q3.(c) Time as a response to Supplem
+supplem_model <- lm(TIME ~ SUPPLEM, data = InData)
+supplem_model
+summary(supplem_model)
+
+# Q4. Multiple regression
+complete_model <- lm(TIME ~ PROTEIN + ANTIBIO + SUPPLEM, data=InData)
+complete_model
+summary(complete_model)
+
+
+# Q5. Residuals versus Fitted Values
+plot(complete_model)
+
+# Q9. Hypothesis Test
+library(car)
+c1 <- c(1, 0, 0, 0)
+lht(complete_model, c1, rhs = c(0))
