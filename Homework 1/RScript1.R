e85e6f9dad573311db3f79900fbbfdd474b807ff
Vignesh M Pagadala <vignesh.pagadala@colostate.edu>
2019-02-11 18:27:29 -0700

    Commit

--- Homework 1/RScript1.R
+++ Homework 1/RScript1.R
@@ -1,3 +1,16 @@
 InData <- read.csv("/home/vignesh_pagadala/Desktop/STAT512/Homework 1/OTT_Final/ASCII-comma/CH12/ex12-53.txt", quote = " ' ", row.names = 1)
 InData
 # This is a test comment.
+
+# Q1. Pairwise Scatterplot
+pairs(InData, gap = 0, pch = "o")
+
+# Q2. Pearson Correlation Coefficient
+cor(InData, method = c('pearson'))
+
+# Q3. Linear regression between time and every other variable.
+
+# Q3.(a) Time as a response to Protein
+protein_model <- lm(TIME ~ PROTEIN, data = InData)
+protein_model
+summary(protein_model)
