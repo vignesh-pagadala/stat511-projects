03707a492d56da1d3895dd1841b88e6f7c7722f2
Vignesh M Pagadala <vignesh.pagadala@colostate.edu>
2019-04-23 16:58:37 -0600

    Commit

--- Homework_5/RScript1.R
+++ Homework_5/RScript1.R
@@ -4,10 +4,31 @@ library(ggplot2)
 
 InData <- read.csv("/home/vignesh_pagadala/Desktop/Course Work/STAT512/Homework_5/CKheart.csv", header = TRUE)
 InData
+CKheart <- InData
+Model1 <- glm(cbind(withHA, withoutHA) ~ CK, family = binomial(link = "logit"), data = InData)
+summary(Model1)
 
+CKheart$rate <- CKheart$withHA/(CKheart$withHA + CKheart$withoutHA)
+NewData <- seq(10, 500, 1)
+phat <- predict(Model1, list(CK = NewData), type = "response")
+plot(rate ~ CK, data = CKheart)
+lines(phat ~ NewData) 
 
-#InData$percentHA <- InData$withHA/(InData$withHA + InData$withoutHA)
-#plot(percentHA ~ CK, data = InData)
+exp(Model1$coef)
+exp(confint(Model1))
 
-#Model1 <- glm(cbind(withHA, withoutHA) ~ CK, family = binomial(link = "logit"), data = In#Data)
-#summary(Model1)
+NullModel <- glm(cbind(withHA, withoutHA) ~ 1, family = binomial(link = "logit"), data = CKheart)
+1-logLik(Model1)/logLik(NullModel)
+
+library(MASS)
+probs <- seq(0.1, 0.9, 0.05)
+ld <- dose.p(Model1, cf = 1:2, p = probs)
+ld
+
+BirthData <- read.csv("/home/vignesh_pagadala/Desktop/Course Work/STAT512/Homework_5/birthweight.csv")
+str(BirthData)
+
+BirthData$race <- as.factor(BirthData$race)
+BirthData$smoke <- as.factor(BirthData$smoke)
+str(BirthData)
+
