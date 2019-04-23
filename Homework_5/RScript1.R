4393805fd5b36dfd821a017eaa440007a8bfb276
Vignesh M Pagadala <vignesh.pagadala@colostate.edu>
2019-04-23 08:54:29 -0600

    Commit

--- Homework_5/RScript1.R
+++ Homework_5/RScript1.R
@@ -5,8 +5,9 @@ library(ggplot2)
 InData <- read.csv("/home/vignesh_pagadala/Desktop/Course Work/STAT512/Homework_5/CKheart.csv", header = TRUE)
 InData
 
-InData$percentHA <- InData$withHA/(InData$withHA + InData$withoutHA)
-plot(percentHA ~ CK, data = InData)
 
-Model1 <- glm(cbind(withHA, withoutHA) ~ CK, family = binomial(link = "logit"), data = InData)
-summary(Model1)
+#InData$percentHA <- InData$withHA/(InData$withHA + InData$withoutHA)
+#plot(percentHA ~ CK, data = InData)
+
+#Model1 <- glm(cbind(withHA, withoutHA) ~ CK, family = binomial(link = "logit"), data = In#Data)
+#summary(Model1)
